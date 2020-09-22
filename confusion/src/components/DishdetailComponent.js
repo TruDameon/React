b4240e60538e9460fd9fe09fdd4a33d3156ec5b9
Dameon <shubhamsingh0602@gmail.com>
2020-09-22 21:48:20 +0530

    Single page website

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -2,7 +2,8 @@
 // Converted into functional component - A component just used for presentation and contains no state properties or constructor
 
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderComments({ comments }) {
     const commments2s = comments.map((c) => {
@@ -20,7 +21,7 @@ function RenderComments({ comments }) {
     return commments2s;
 }
 
-function RenderDish({ dish }) {
+function RenderDish({ dish, comments }) {
     return (
         <div className="row">
             <div className="col-12 col-md-5 m-1">
@@ -38,7 +39,7 @@ function RenderDish({ dish }) {
                         Comments
                     </h4>
                 </header>
-                <RenderComments comments={dish.comments} />
+                <RenderComments comments={comments} />
             </div>
         </div>
     )
@@ -48,7 +49,18 @@ function Dishdetail(props) {
     if (props.dish != null) {
         return (
             <div className="container">
-                <RenderDish dish={props.dish} />
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr/>
+                    </div>
+                </div>
+                <RenderDish dish={props.dish} comments={props.comments}/>
             </div>
         );
     } else {
