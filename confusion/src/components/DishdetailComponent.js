dc1332d7c19142c202eb12aa47cebaf71917000a
Dameon <shubhamsingh0602@gmail.com>
2020-09-25 16:42:47 +0530

    Fetch From Server

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form'; 
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -123,7 +124,7 @@ function RenderDish({ dish, comments, addComment, dishId }) {
         <div className="row">
             <div className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                    <CardImg width="100%" object src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
