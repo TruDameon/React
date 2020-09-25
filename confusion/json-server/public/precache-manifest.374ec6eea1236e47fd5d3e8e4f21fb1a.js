6698d91bc29727a821de1450b9e4823190042e8b
Dameon <shubhamsingh0602@gmail.com>
2020-09-25 19:53:57 +0530

    transferred minified & uglyfied files to json-server folder to serve it directly form the server

---
+++ confusion/json-server/public/precache-manifest.374ec6eea1236e47fd5d3e8e4f21fb1a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7f6b6e1bfa4760508a0fa19e4772f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0766eb2974abf31f359f",
+    "url": "/static/css/2.b0e1eb24.chunk.css"
+  },
+  {
+    "revision": "0587b69c7a17ffcad35d",
+    "url": "/static/css/main.e172d65f.chunk.css"
+  },
+  {
+    "revision": "0766eb2974abf31f359f",
+    "url": "/static/js/2.d0ef2ac3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d0ef2ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0587b69c7a17ffcad35d",
+    "url": "/static/js/main.cefe7ccd.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
