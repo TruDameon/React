ccc2356ed7f6cce5b756d25272bb0ca0b57a15ee
Dameon <shubhamsingh0602@gmail.com>
2020-10-21 18:22:40 +0530

    Course Complete

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,26 +1,26 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { createForms } from "react-redux-form";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-import { InitialFeedback } from "./forms";
-import { Dishes } from "./dishes";
-import { Comments } from "./comments";
-import { Promotions } from "./promotions";
-import { Leaders } from "./leaders";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form'; // Lets us bring form state into store, so form state can persist across views (contrast to LocalForm)
+import { Comments } from './comments';
+import { Dishes } from './dishes';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'; // Logs action nicely to console (prev state, action, next state)
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore = () => {
-  const store = createStore(
-    combineReducers({
-      dishes: Dishes,
-      comments: Comments,
-      promotions: Promotions,
-      leaders: Leaders,
-      ...createForms({
-        feedback: InitialFeedback
-      })
-    }),
-    applyMiddleware(thunk, logger)
-  );
+    const store = createStore(
+        combineReducers({
+            comments: Comments,
+            dishes: Dishes,
+            leaders: Leaders,
+            promotions: Promotions,
+            ...createForms({
+                feedback: InitialFeedback,
+            }),
+        }),
+        applyMiddleware(thunk, logger)
+    );
 
-  return store;
-};
+    return store;
+}
