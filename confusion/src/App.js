7ff41c801aa3bdd82daa86d33952f64a6c4d0950
Dameon <shubhamsingh0602@gmail.com>
2020-10-21 18:11:33 +0530

    Completed Course

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import Main from './components/MainComponent';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+import Main from "./components/MainComponent";
+import "./App.css";
 
 const store = ConfigureStore();
 
-class App extends React.Component {
-
+class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <div>
+          <div className="App">
             <Main />
           </div>
         </BrowserRouter>
